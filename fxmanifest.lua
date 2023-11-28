8edfb70787d6fac507115bdcdadfd658767e05af
rockysx27 <128807992+rockysx27@users.noreply.github.com>
2023-11-28 05:36:14 +0000

    ae

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version  'cerulean'
+game        'gta5'
+description 'real-blood'
+version     '1.0.0'
+
+files {
+    'stream/**/*.ytd',
+    'stream/**/*.ypt',
+    'stream/*.ymt'
+}
+
+data_file 'PED_DAMAGE_APPEND_FILE'  'stream/bacb928c-d2df-4695-8a6d-90f1510ca9e9/peddamage.xml'
+data_file 'PED_DAMAGE_APPEND_FILE'  'stream/8b5cc2c4-0c38-4878-af32-0cea1818daea/peddamage.xml'
+
+data_file 'SCRIPTFX_FILE' 'stream/9d4c5623-df19-424b-afd4-bf7eb37e4f82/liquidfx.dat'
+data_file 'SCRIPTFX_FILE' 'stream/043e4efd-fe0d-4d48-b2f0-b895a9b31cdc/liquidfx.dat'
+
+data_file 'AMB_PROCEDURAL_BLOOD_FILE' 'stream/30ca399d-9225-4941-aeb8-6c7da21bc612/weaponfx.dat'
+data_file 'AMB_PROCEDURAL_BLOOD_FILE' 'stream/52fe4dbf-25c2-45c1-b98a-73ad5656bb65/weaponfx.dat'
+
+data_file 'AMB_PROCEDURAL_BLOOD_FILE' 'stream/9111d70d-5ca1-41d9-8c84-48b0222db580/bloodfx.dat'
+data_file 'AMB_PROCEDURAL_BLOOD_FILE' 'stream/c7efb55d-9fc7-4ba8-a32d-55f5766d50c1/bloodfx.dat'
+
+data_file 'AMB_PROCEDURAL_BLOOD_FILE' 'stream/f4b93f71-f785-4e8b-a25b-db9e20c22aed/decals.dat'
+data_file 'AMB_PROCEDURAL_BLOOD_FILE' 'stream/0a689fb7-9824-4323-a3a0-46327d5d5dc1/decals.dat'
